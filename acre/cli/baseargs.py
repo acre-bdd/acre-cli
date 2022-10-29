3ad1662f7daf4db2c1e7e07f549cf6a826f8ca52
Claudio Klingler <ck@realtime-projects.com>
2022-10-29 13:11:26 +0200

    fix acrepath import.

--- acre/cli/baseargs.py
+++ acre/cli/baseargs.py
@@ -1,8 +1,3 @@
-from .acrepath import AcrePath
-
-
 def add_to(parser):
     """ add the common arguments to the argument parser """
     parser.add_argument('--debug', action='store_true', help='enable debug logging', default=False)
-    parser.add_argument('--acrepath', action=AcrePath,
-                        help=f'set to acre virtual environment, default is {AcrePath.path}')
