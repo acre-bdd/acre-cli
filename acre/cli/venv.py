bfcc34a8753bf029c6720ca9e41004e725859f9e
Claudio Klingler <ck@realtime-projects.com>
2022-10-29 13:14:05 +0200

    fix acrepath import.

--- acre/cli/venv.py
+++ acre/cli/venv.py
@@ -1,11 +1,9 @@
 import subprocess
 
 from acre.cli import log
-from acre.cli.acrepath import AcrePath
 
 
 def run(command):
-    cmd = f". {AcrePath.path}/bin/activate && {command}"
     cmd = f"{command}"
     log.debug(f"running: {command}")
     subprocess.run(cmd, shell=True)
